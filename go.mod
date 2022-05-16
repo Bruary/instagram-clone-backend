df4c65bed8c97f80fd06c27bd5f505d07942f034
Abubakir Kais <bakir_janabi@yahoo.com>
2022-05-16 13:51:22 +0400

    Adding the initial main.go with db connection

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Bruary/instagram-clone-backend
 
 go 1.16
+
+require (
+	github.com/gofiber/fiber/v2 v2.33.0 // indirect
+	github.com/lib/pq v1.10.5 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"github.com/Bruary/instagram-clone-backend/db"
+	"github.com/gofiber/fiber/v2"
+)
 
+func main() {
+	db.SetpUpDbConnection()
+
+	app := fiber.New()
+
+	app.Post("/api/v1/user", func(c *fiber.Ctx) error {
+		return nil
+	})
+
+	app.Put("/api/v1/user", func(c *fiber.Ctx) error {
+		return nil
+	})
+
+	app.Get("/api/v1/user", func(c *fiber.Ctx) error {
+		return nil
+	})
 }
